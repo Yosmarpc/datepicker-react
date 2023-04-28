d6aba297eed51d943b4ece7f9d5fb1b337095565
Yosmar Hinestroza <yosmarhinestroza@MacBook-Air-de-Yosmar.local>
2023-04-28 18:04:02 -0400

    base inicial date pikcer

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Filter from './components/Filter'
+import { LocalizationProvider } from '@mui/x-date-pickers'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Filter />
+      </LocalizationProvider>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
